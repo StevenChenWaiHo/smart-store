02745e532b9c1e2d7ee66b77d7ec7e1fd0071be7
StevenChenWaiHo <stevenchenwaiho@gmail.com>
2023-10-16 03:12:01 +0100

    added add item no code button and change render after swipe up

--- src/screens/AddItemScreen/index.js
+++ src/screens/AddItemScreen/index.js
@@ -5,9 +5,11 @@ import React, { useState, useEffect, useRef, useMemo, useCallback } from 'react'
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import BottomSheet from '@gorhom/bottom-sheet';
-import { Camera, CameraType } from 'expo-camera';
+import { Camera, CameraType }
+    from 'expo-camera';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { styles } from '../../styles/global/globalStyle'
+import defaultImage from '../../assets/images/noImage.png'
 
 export default function AddItemScreen() {
     const [hasPermission, setHasPermission] = Camera.useCameraPermissions();
@@ -16,22 +18,73 @@ export default function AddItemScreen() {
     const [itemName, setItemName] = useState('');
     const [date, setDate] = useState(new Date());
     const [dateString, setDateString] = useState('');
-    const [image, setImage] = useState();
+    const [image, setImage] = useState('https://www.freeiconspng.com/uploads/no-image-icon-11.PNG');
     const [showDatePicker, setShowDatePicker] = useState(false);
+    const [itemConfirmed, setItemConfirmed] = useState(false);
+    const [takingPhoto, setTakingPhoto] = useState(false);
 
-    // Bottom Nav Bar
-    const Tab = createMaterialTopTabNavigator();
-    const mode = ['SCAN', 'ITEM', 'LIST']
+    const handleItemConfirm = () => {
+        setItemConfirmed(true);
+        bottomSheetRef.current.expand();
+    }
+
+    const handleNoCodeAddItem = () => {
+        setItemConfirmed(true);
+        bottomSheetRef.current.expand();
+    }
 
     // Bottom Sheet
     const bottomSheetRef = useRef(null);
-    const snapPoints = useMemo(() => ['25%', '100%'], []);
+    const snapPoints = useMemo(() => itemConfirmed ? ['100%'] : ['25%', '100%'], [itemConfirmed]);
 
     const handleBottomSheetChanged = (pos) => {
+        console.log('Modal Current Position: ', pos)
+        if (itemConfirmed && takingPhoto) {
+            return;
+        }
         // Discard scanned item after bottom sheet is closed
         if (pos === -1) {
             setScanned(false);
+            setItemConfirmed(false)
+            return;
         }
+        // Item Confirmed when swipe up after scanned
+        if (pos === 1 && scanned) {
+            setItemConfirmed(true)
+            return;
+        }
+    }
+    // Camera
+    const cameraRef = useRef(null);
+
+    const takePicture = async () => {
+        const picture = await cameraRef.current.takePictureAsync();
+        setImage(picture.uri);
+        setTakingPhoto(false);
+
+        if (!itemConfirmed) {
+            console.log('Item not confirmed when taking Photo')
+            return
+        }
+
+        bottomSheetRef.current.expand();
+    }
+
+    const handleBarCodeScanned = (barcode) => {
+        setScanned(true);
+        setBarcode(barcode);
+        bottomSheetRef.current.snapToIndex(0)
+    };
+
+    if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+    const handleChangePhotoButton = () => {
+        setTakingPhoto(true);
+        bottomSheetRef.current.close();
     }
 
     // Date
@@ -52,25 +105,101 @@ export default function AddItemScreen() {
         }
     }
 
-    // Camera
-    const cameraRef = useRef(null);
-
-    const takePicture = async () => {
-        const picture = await cameraRef.current.takePictureAsync();
-        setImage(picture.uri);
+    const RenderDatePicker = () => {
+        if (Platform.OS === 'android') {
+            // DatePicker for android
+            return (
+                <>
+                    <Pressable
+                        onPress={toggleDatePicker}>
+                        <TextInput
+                            placeholder="Enter Date here"
+                            style={styles.input}
+                            value={dateString}
+                            editable={false}
+                            onPressIn={toggleDatePicker} />
+                    </Pressable>
+                    {
+                        showDatePicker &&
+                        (<DateTimePicker
+                            mode='date'
+                            display={'calendar'}
+                            value={date}
+                            onChange={onChangeDate} />)
+                    }
+                </>);
+        } else if (Platform.OS === 'ios') {
+            // DatePicker for ios
+            return (
+                <DateTimePicker
+                    mode='date'
+                    display='compact'
+                    value={date}
+                    onChange={onChangeDate}
+                    style={styles.datePicker} />)
+        }
     }
 
-    const handleBarCodeScanned = (barcode) => {
-        setScanned(true);
-        setBarcode(barcode);
-        bottomSheetRef.current.snapToIndex(0)
-    };
+    const RenderBottomSheetWhenScanned = () => {
+        return (
+            <View style={styles.modalContainer}>
+                <View style={{ flexDirection: 'row' }}>
+                    <View style={{ ...styles.topCenteredContainer, flex: 2 }}>
+                        <TouchableOpacity
+                            style={styles.bottomSheetImageContainer}>
+                            <Image
+                                source={{ uri: image }}
+                                style={styles.bottomSheetImage} />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{ ...styles.topLeftContainer, flex: 4 }}>
+                        <Text>Code: {barcode.data}</Text>
+                        <Text>Item Name:</Text>
+                        <TextInput
+                            placeholder="Enter item name here"
+                            style={styles.input}
+                            value={itemName}
+                        />
 
-    if (hasPermission === null) {
-        return <Text>Requesting for camera permission</Text>;
+                    </View>
+                </View>
+            </View>)
     }
-    if (hasPermission === false) {
-        return <Text>No access to camera</Text>;
+
+    const RenderBottomSheetWhenItemConfirmed = () => {
+        return (
+            <>
+                <View style={styles.topLeftContainer}>
+                    <Button
+                        onPress={handleChangePhotoButton}
+                        title="Edit Image" />
+                    <TouchableOpacity
+                        style={styles.bottomSheetImageContainer}
+                        onPress={handleChangePhotoButton}>
+                        <Image
+                            source={{ uri: image }}
+                            style={styles.bottomSheetImage} />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.topLeftContainer}>
+                    <Text>Code: {barcode.data}</Text>
+                    <Text>Item Name:</Text>
+                    <TextInput
+                        placeholder="Enter item name here"
+                        style={styles.input}
+                        value={itemName}
+                    />
+                    <Text>Date:</Text>
+                    <RenderDatePicker />
+                </View>
+
+                <View style={styles.bottomRightContainer}>
+                    {!itemConfirmed && <Button
+                        onPress={handleItemConfirm}
+                        title="YES" />}
+                </View>
+            </>
+        )
     }
 
     return (
@@ -83,63 +212,36 @@ export default function AddItemScreen() {
                     onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
                     ref={cameraRef}>
                     {/* Take Photo Button */}
-                    {scanned &&
+                    {takingPhoto &&
                         <TouchableOpacity
                             onPress={takePicture}
-                            style={styles.bottomSheetImage}
-                        />}
+                            style={styles.takePhotoButton}>
+                        </TouchableOpacity>}
+                    {!itemConfirmed &&
+                        <TouchableOpacity
+                            onPress={handleNoCodeAddItem}
+                            style={styles.addItemButton}>
+                            <Image
+                            source={{ uri: 'https://icons.iconarchive.com/icons/iconsmind/outline/512/Add-icon.png' }}
+                            style={styles.bottomSheetImage} />
+                        </TouchableOpacity>}
 
                 </Camera>
+
+
+                <BottomSheet
+                    ref={bottomSheetRef}
+                    index={-1}
+                    snapPoints={snapPoints}
+                    enableContentPanningGesture={true}
+                    enablePanDownToClose={true}
+                    onChange={handleBottomSheetChanged}>
+                    {itemConfirmed ? (<RenderBottomSheetWhenItemConfirmed />) : scanned ? (<RenderBottomSheetWhenScanned />) : <></>}
+                </BottomSheet>
+
             </View>
 
-            <BottomSheet
-                ref={bottomSheetRef}
-                index={-1}
-                snapPoints={snapPoints}
-                enableContentPanningGesture={true}
-                enablePanDownToClose={true}
-                onChange={handleBottomSheetChanged}>
-                <View style={styles.modalContainer}>
-                    <View style={{ flexDirection: 'row' }}>
-                        <View style={{ ...styles.topCenteredContainer, flex: 5 }}>
-                            <TouchableOpacity>
-                                <Image
-                                    source={{ uri: image }}
-                                    style={{ height: 100, width: 100 }} />
-                            </TouchableOpacity>
-                        </View>
-                        <View style={{ ...styles.topLeftContainer, flex: 4 }}>
-                            <Text>Code: {barcode.data}</Text>
-                            <Text>Item Name:</Text>
-                            <TextInput
-                                placeholder="Enter item name here"
-                                style={styles.input}
-                                value={itemName}
-                            />
-                            <Text>Date:</Text>
-                            {!showDatePicker &&
-                                (<Pressable
-                                    onPress={toggleDatePicker}>
-                                    <TextInput
-                                        placeholder="Enter Date here"
-                                        style={styles.input}
-                                        value={dateString}
-                                        editable={false}
-                                        onPressIn={toggleDatePicker}
-                                    />
-                                </Pressable>)}
 
-                            {showDatePicker &&
-                                (<DateTimePicker
-                                    mode='date'
-                                    display='spinner'
-                                    value={new Date()}
-                                    onChange={onChangeDate}
-                                    style={styles.datePicker} />)}
-                        </View>
-                    </View>
-                </View>
-            </BottomSheet>
         </GestureHandlerRootView>
     );
 }
--- src/styles/global/globalStyle.js
+++ src/styles/global/globalStyle.js
@@ -1,50 +1,79 @@
 import { StyleSheet } from "react-native";
+import { Platform } from "react-native";
 
 export const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: 'lightgrey',
-      alignItems: 'center',
-      justifyContent: 'center',
+  container: {
+    flex: 1,
+    backgroundColor: 'lightgrey',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  topLeftContainer: {
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    margin: '2%'
+  },
+  topCenteredContainer: {
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    margin: '2%'
+  },
+  bottomRightContainer: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    marginRight: '10%'
+  },
+  datePicker: {
+    height: 120,
+    marginTop: -40,
+    marginLeft: -10,
+  },
+  modalContainer: {
+    height: '100%',
+    width: '100%',
+    marginLeft: '5%',                              
+    marginRight: '5%'
+  },
+  shadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    topLeftContainer: {
-      justifyContent: 'flex-start',
-      alignItems: 'flex-start',
-    },
-    topCenteredContainer: {
-      justifyContent: 'flex-start',
-      alignItems: 'center',
-    },
-    datePicker: {
-      height: 120,
-      marginTop: -10,
-      margin: '2%',
-    },
-    modalContainer: {
-      height: '100%',
-      width: '100%',
-    },
-    shadow: {
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 3.84,
-      elevation: 5,
-    },
-    camera: {
-      height: '100%',
-      width: '100%',
-      justifyContent: 'flex-end',
-      alignItems: 'center',
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  camera: {
+    height: '100%',
+    width: '100%',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  takePhotoButton: {
+    width: 70,
+    height: 70,
+    bottom: 0,
+    borderRadius: 50,
+    backgroundColor: '#fff'
+  },
+  addItemButton: {
+    width: 70,
+    height: 70,
+    bottom: 0,
+    borderRadius: 50,
+    backgroundColor: '#fff',
+  },
+  bottomSheetImageContainer: {
+    width: '100%',
+    height: '45%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   bottomSheetImage: {
-      width: 70,
-      height: 70,
-      bottom: 0,
-      borderRadius: 50,
-      backgroundColor: '#fff'
-    }
-  });
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
