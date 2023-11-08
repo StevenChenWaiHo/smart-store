cb99f8f99f60e84b7c9e7ca84a600518ccc04160
StevenChenWaiHo <stevenchenwaiho@gmail.com>
2023-11-08 16:39:17 +0000

    checkAllTablesSchema

--- src/data/SQLite/updateDatabase.js
+++ src/data/SQLite/updateDatabase.js
@@ -24,15 +24,15 @@ export default function updateDatabase({ debug }) {
         debugLog({ debug, message: 'INIT ALL TABLES' })
         db.transaction(tx => {
             // Create the tables with current schema
-            schemaToSQL(dbUpgrade.currentSchema).forEach((table) => {
+            schemaToSQL(dbUpgrade.baseSchema).forEach((table) => {
                 tx.executeSql(table, null,
-                    (txObj, result) => { },
-                    (txObj, error) => { debugLog({debug, message: error});  return false })
+                    (txObj, result) => { debugLog({ debug, message: `${table}: ${JSON.stringify(result)}` }) },
+                    (txObj, error) => { debugLog({ debug, message: error }); return false })
             })
             // Add version number to version table
-            tx.executeSql("INSERT INTO version (versionNumber) values (?)", [dbUpgrade.version],
-                (txObj, result) => { return true },
-                (txObj, error) => { debugLog({debug, message: error}); return false })
+            tx.executeSql("INSERT INTO version (versionNumber) values (?)", [1],
+                (txObj, result) => { debugLog({ debug, message: `Add newest version number: ${JSON.stringify(result)}` }); return true },
+                (txObj, error) => { debugLog({ debug, message: error }); return false })
         })
     }
 
@@ -50,15 +50,16 @@ export default function updateDatabase({ debug }) {
                             db.transaction(tx => {
                                 tx.executeSql(dbUpgrade.upgrades[`to_v${i}`],
                                     [],
-                                    (txObj, result) => { },
+                                    (txObj, result) => { debugLog({ debug, message: `${dbUpgrade.upgrades[`to_v${i}`]}: ${result}` }) },
                                     (txObj, error) => debugLog({ debug, message: error }))
                                 tx.executeSql(`INSERT INTO version (versionNumber) values (?)`, [i],
                                     [],
-                                    (txObj, result) => { },
+                                    (txObj, result) => { debugLog({ debug, message: `Add Upgraded version: ${result}` }) },
                                     (txObj, error) => debugLog({ debug, message: error }))
                             })
                         }
                     }
+                    checkFinalTableSchema()
                 },
                 (txObj, error) => { debugLog({ debug, message: error }) })
         })
@@ -83,6 +84,32 @@ export default function updateDatabase({ debug }) {
         })
     }
 
+    const checkFinalTableSchema = () => {
+        db.transaction(tx => {
+            tx.executeSql("PRAGMA table_info(version);", [],
+                (txObj, result) => { debugLog({ debug, message: `version table: ${result.rows._array.map((json => JSON.stringify(json)))}` }) },
+                (txObj, error) => { debugLog({ debug, message: error }) })
+            tx.executeSql("PRAGMA table_info(list)", [],
+                (txObj, result) => { debugLog({ debug, message: `list table: ${result.rows._array.map((json => JSON.stringify(json)))}` }) },
+                (txObj, error) => { debugLog({ debug, message: error }) })
+            tx.executeSql("PRAGMA table_info(barcodeMap)", [],
+                (txObj, result) => { debugLog({ debug, message: `barcodeMap table: ${result.rows._array.map((json => JSON.stringify(json)))}` }) },
+                (txObj, error) => { debugLog({ debug, message: error }) })
+
+            tx.executeSql("SELECT * FROM version", [],
+                (txObj, result) => { debugLog({ debug, message: `version table: ${result.rows._array.map((json => JSON.stringify(json)))}` }) },
+                (txObj, error) => { debugLog({ debug, message: error }) })
+            tx.executeSql("SELECT * FROM list", [],
+                (txObj, result) => { debugLog({ debug, message: `list table: ${result.rows._array.map((json => JSON.stringify(json)))}` }) },
+                (txObj, error) => { debugLog({ debug, message: error }) })
+            tx.executeSql("SELECT * FROM barcodeMap", [],
+                (txObj, result) => { debugLog({ debug, message: `barcodeMap table: ${result.rows._array.map((json => JSON.stringify(json)))}` }) },
+                (txObj, error) => { debugLog({ debug, message: error }) })
+        })
+    }
+
+    deleteAllTables()
+
     db.transaction(tx => {
         tx.executeSql(`CREATE TABLE IF NOT EXISTS version (versionNumber INTEGER)`, [],
             (txObj, results) => { },
@@ -97,23 +124,22 @@ export default function updateDatabase({ debug }) {
                 debugLog({ debug, message: `Current Version: ${version}` })
                 // version table doesn't exist or it does not have any entry
                 if (!version) {
-                    if (initialiseAllTables()) {
-                        debugLog({ debug, message: 'INIT ALL TABLES SUCCESS' })
-                        return
-                    } else {
-                        /* Cannot initialise tables. It might be becuase the app is 
-                        in a version which didnt have the version table but have list
-                        and barcodeMap table, which is database version 1*/
-                        debugLog({ debug, message: 'INIT ALL TABLES FAILED' })
-                        tx.executeSql("INSERT INTO version (versionNumber) values (?)", [1],
-                            (txObj, result) => { },
-                            (txObj, error) => { debugLog({ debug, message: error }) })
-                    }
+                    initialiseAllTables()
+                    /* Cannot initialise tables. It might be becuase the app is 
+                    in a version which didnt have the version table but have list
+                    and barcodeMap table, which is database version 1*/
+                    tx.executeSql("INSERT INTO version (versionNumber) values (?)", [1],
+                        (txObj, result) => { },
+                        (txObj, error) => { debugLog({ debug, message: error }) })
+                    checkFinalTableSchema()
+
                 }
 
                 upgradeDatabaseToLatestVersion()
+
             },
             (txObj, error) => { debugLog({ debug, message: error }) }
         )
     })
+    checkFinalTableSchema()
 }
