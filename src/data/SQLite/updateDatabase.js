dc7082b7e1b6993a25a17af42b92824d63134f72
StevenChenWaiHo <stevenchenwaiho@gmail.com>
2023-11-08 09:49:55 +0000

    upgrade table for version with no version table

--- src/data/SQLite/updateDatabase.js
+++ src/data/SQLite/updateDatabase.js
@@ -20,6 +20,7 @@ export default function updateDatabase() {
 
     // Create tables with current schema return current version number if success
     const initialiseAllTables = () => {
+        console.log('INIT ALL TABLES')
         db.transaction(tx => {
             // Create the tables with current schema
             schemaToSQL(dbUpgrade.currentSchema).forEach((table) => {
@@ -29,34 +30,61 @@ export default function updateDatabase() {
             })
             // Add version number to version table
             tx.executeSql("INSERT INTO version (versionNumber) values (?)", [dbUpgrade.version],
-                (txObj, result) => { return dbUpgrade.version },
+                (txObj, result) => { return true },
                 (txObj, error) => console.log(error))
         })
-        return null
+        return false
     }
 
-    // DELETE ALL DATABASE
+    const upgradeDatabaseToLatestVersion = () => {
+        db.transaction(tx => {
+            tx.executeSql("SELECT max(versionNumber) FROM version", [],
+                (txObj, results) => {
+                    const currentVersion = results.rows._array[0]['max(versionNumber)'];
+                    const latestVersion = dbUpgrade.version
+                    if (currentVersion < latestVersion) {
+                        console.log(`Database upgrading from version ${currentVersion} to version ${latestVersion}...`)
+                        alert(`Database upgrading from version ${currentVersion} to version ${latestVersion}...`)
+                        for (let i = currentVersion + 1; i <= latestVersion; i++) {
+                            db.transaction(tx => {
+                                tx.executeSql(dbUpgrade.upgrades[`to_v${i}`],
+                                    [],
+                                    (txObj, result) => { },
+                                    (txObj, error) => alert(error))
+                                tx.executeSql(`INSERT INTO version (versionNumber) values (?)`, [i],
+                                    [],
+                                    (txObj, result) => { },
+                                    (txObj, error) => alert(error))
+                            })
+                        }
+                    }
+                })
+        })
+    }
+
+    const deleteAllTables = () => {
+        db.transaction(tx => {
+            tx.executeSql(`DROP TABLE IF EXISTS version`,
+                [],
+                (txObj, result) => { console.log('DELETE version') },
+                (txObj, error) => console.log(error))
+
+            tx.executeSql(`DROP TABLE IF EXISTS list`,
+                [],
+                (txObj, result) => { console.log('DELETE list') },
+                (txObj, error) => console.log(error))
+
+            tx.executeSql(`DROP TABLE IF EXISTS barcodeMap`,
+                [],
+                (txObj, result) => { console.log('DELETE barcodeMap') },
+                (txObj, error) => console.log(error))
+        })
+    }
+
+
     db.transaction(tx => {
-        tx.executeSql(`DROP TABLE IF EXISTS version`,
-            [],
-            (txObj, result) => { console.log('DELETE version') },
-            (txObj, error) => console.log(error))
-
-        tx.executeSql(`DROP TABLE IF EXISTS list`,
-            [],
-            (txObj, result) => { console.log('DELETE list') },
-            (txObj, error) => console.log(error))
-
-        tx.executeSql(`DROP TABLE IF EXISTS barcodeMap`,
-            [],
-            (txObj, result) => { console.log('DELETE barcodeMap') },
-            (txObj, error) => console.log(error))
-    })
-
-    db.transaction(tx => {
-        tx.executeSql(`CREATE TABLE IF NOT EXISTS version (
-    versionNumber INTEGER)`, [],
-            (txObj, results) => { console.log(results) },
+        tx.executeSql(`CREATE TABLE IF NOT EXISTS version (versionNumber INTEGER)`, [],
+            (txObj, results) => { },
             (txObj, error) => console.log(error))
     })
 
@@ -65,36 +93,22 @@ export default function updateDatabase() {
         tx.executeSql("SELECT max(versionNumber) FROM version", [],
             (txObj, results) => {
                 const version = results.rows._array[0]['max(versionNumber)'];
-                console.log(version)
+                console.log('Current Version: ', version)
                 // version table doesn't exist or it does not have any entry
                 if (!version) {
-                    console.log('INIT ALL TABLES')
-                    // Add base version number to version table
-                    tx.executeSql("INSERT INTO version (versionNumber) values (?)", [1],
-                        (txObj, result) => { },
-                        (txObj, error) => console.log(error))
-                    
                     if (initialiseAllTables()) {
                         return
+                    } else {
+                        /* Cannot initialise tables. It might be becuase the app is 
+                        in a version which didnt have the version table but have list
+                        and barcodeMap table, which is database version 1*/
+                        tx.executeSql("INSERT INTO version (versionNumber) values (?)", [1],
+                            (txObj, result) => { },
+                            (txObj, error) => console.log(error))
                     }
                 }
-                // Upgrade if outdated
-                const latestVersion = dbUpgrade.version
-                if (version < latestVersion) {
-                    alert(`Database upgrading from version ${version} to version ${latestVersion}...`)
-                    for (let i = version + 1; i <= latestVersion; i++) {
-                        db.transaction(tx => {
-                            tx.executeSql(dbUpgrade.upgrades[`to_v${i}`],
-                                [],
-                                (txObj, result) => { },
-                                (txObj, error) => alert(error))
-                            tx.executeSql(`INSERT INTO version (versionNumber) values (?)`, [i],
-                                [],
-                                (txObj, result) => { },
-                                (txObj, error) => alert(error))
-                        })
-                    }
-                }
+
+                upgradeDatabaseToLatestVersion()
             })
     })
 }
