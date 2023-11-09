ce1f14291d0b6aa19b9eebb1bda8495f7fd34e13
StevenChenWaiHo <stevenchenwaiho@gmail.com>
2023-11-09 23:41:42 +0000

    debug day difference

--- src/data/date/dateDifferenceInDays.js
+++ src/data/date/dateDifferenceInDays.js
@@ -1,9 +1,19 @@
+import debugLog from "../utils/debug/debugLog"
+
 export default function dateDifferenceInDays(oldDate, newDate) {
+    const msToDay = (1000 * 60 * 60 * 24)
     oldDate.setHours(8)
     oldDate.setMinutes(0)
     oldDate.setSeconds(0)
     newDate.setHours(8)
     newDate.setMinutes(0)
     newDate.setSeconds(0)
-    return parseInt(Math.ceil((oldDate - newDate) / (1000 * 60 * 60 * 24)));
+    debugLog({
+        debugMode: true, message: `
+        ${oldDate.toString()} ${newDate.toString()}
+     ${parseInt(Math.ceil(oldDate / msToDay))} ${parseInt(Math.ceil(newDate / msToDay))}
+     ${Math.ceil(oldDate / msToDay)} ${Math.ceil(newDate / msToDay)}
+     ${oldDate/ msToDay} ${newDate/ msToDay}`
+    })
+    return parseInt(Math.ceil(oldDate/ msToDay)) - parseInt(Math.ceil(newDate/ msToDay));
 }
