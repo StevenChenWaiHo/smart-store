d00e15348d59912a7b575ae3732f6d3a93c0c74b
StevenChenWaiHo <stevenchenwaiho@gmail.com>
2024-04-13 18:12:31 +0100

    remove Transaction Done in transactionHandler

--- src/data/SQLite/utils/transactionHandler.js
+++ src/data/SQLite/utils/transactionHandler.js
@@ -3,6 +3,6 @@ import debugLog from "../../debug/debugLog";
 export default function transactionHandler(db, executions, debugMode = true) {
     db.transaction(executions,
         (txObj, error) => debugLog({ debugMode, message: error }),
-        () => debugLog({ debugMode, message: 'Transaction Done' })
+        () => {}
     )
 }
